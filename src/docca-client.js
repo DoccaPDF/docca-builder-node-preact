d79bb48c59a23ade5b75e802d98aa2f4713df08c
Jason Galea <jason@lecstor.com>
2017-03-26 16:07:39 +1000

    client documentation

--- src/docca-client.js
+++ src/docca-client.js
@@ -1,6 +1,17 @@
 import request from 'request';
 
-export function renderDocument ({ apiUrl, apiKey, doc, images, timeout = 10000, res }) {
+/**
+ * render a document to PDF with docca.io
+ *
+ * @export
+ * @param {String} apiUrl - url of the Docca API server
+ * @param {String} apiKey - API Key for the Docca API server
+ * @param {String} doc - Docca markup document
+ * @param {Array} images - readStreams for images to be posted to the Docca API server
+ * @param {Number} [timeout] - client timeput in miliseconds. Defaults to 10000
+ * @param {Stream} res - the stream to pipe the PDF response to
+ */
+export function renderDocument ({ apiUrl, apiKey, doc, images = [], timeout = 10000, res }) {
   const params = {
     url: `${apiUrl}/render/document`,
     auth: { user: apiKey },
