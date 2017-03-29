224812ac3fe44e2ec479a7e74ec514208b56acd7
Jason Galea <jason@lecstor.com>
2017-03-29 22:51:17 +1000

    change demos curl commands

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,16 +8,17 @@ import Invoice from '../components/invoice';
 
 export default function initRoutes ({ app, log, apiUrl, apiKey, timeout }) {
   app.post('/invoice', (req, res) => {
+    console.log({ body: req.body });
     const invoice = req.body.invoice;
     const doc = render(<Invoice invoice={invoice} />);
     renderDocument({ apiUrl, apiKey, doc, timeout, res });
   });
 
-  app.post('/invoice-local-logo', (req, res) => {
+  app.post('/invoice-demo-logo', (req, res) => {
     const invoice = req.body.invoice;
     const doc = render(<Invoice invoice={invoice} />);
     if (invoice.supplier.logo) {
-      fs.stat(`./image/${invoice.supplier.logo}`, (err, stat) => {
+      fs.stat(`./demo/images/${invoice.supplier.logo}`, (err, stat) => {
         if (err) {
           log.error(err);
           res.status(500);
@@ -26,7 +27,7 @@ export default function initRoutes ({ app, log, apiUrl, apiKey, timeout }) {
           log.error({ image: invoice.supplier.logo }, 'image not found in image directory');
           return res.status(404).send(`Not Found: ${invoice.supplier.logo}`);
         }
-        const images = [fs.createReadStream(`./image/${invoice.supplier.logo}`)];
+        const images = [fs.createReadStream(`./demo/images/${invoice.supplier.logo}`)];
         renderDocument({ apiUrl, apiKey, doc, images, timeout, res });
       });
     } else {
