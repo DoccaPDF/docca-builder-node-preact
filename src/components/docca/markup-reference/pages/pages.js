aca71b107441ffbf554ff33eb56d2f73c5f12dd2
Jason Galea <jason@lecstor.com>
2017-04-02 16:05:35 +1000

    small fix to pages component in markup ref

--- src/components/docca/markup-reference/pages/pages.js
+++ src/components/docca/markup-reference/pages/pages.js
@@ -42,8 +42,8 @@ const Pages = () => (
     <p>Each &lt;page&gt; element will start on a new page in the PDF and may span multiple pages in the resulting PDF document.</p>
     <p>Each &lt;newPage&gt; element will start on a new page and will include the &lt;header&gt; and &lt;footer&gt; of it's parent.</p>
     <p>&lt;header&gt; and &lt;footer&gt; elements are included in each page their parent block is in.</p>
+    <p>&lt;block&gt; elements will not be split unless they are too big to fit on a single page, including any headers or footers.</p>
     <Pre>{pre}</Pre>
-    <p>&lt;block&gt;elements will not be split unless they are too big to fit on a single page, including any headers or footers.</p>
   </newPage>
 );
 
