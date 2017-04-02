eafd1e32a08adc9546d0006cf09d38ea31d6fee6
Jason Galea <jason@lecstor.com>
2017-04-02 15:27:36 +1000

    small edit to pages component in markup ref

--- src/components/docca/markup-reference/pages/pages.js
+++ src/components/docca/markup-reference/pages/pages.js
@@ -39,11 +39,11 @@ const pre = `
 const Pages = () => (
   <newPage id='pages'>
     <Header>Pages</Header>
-    <p>Each &lt;page&gt; element will be split into multiple pages in the generated PDF where required.</p>
-    <p>Each &lt;newPage&gt; element will start on a new page in the PDF.</p>
-    <p>&lt;block&gt;elements will not be split unless they are too big to fit on a single page, including any headers or footers.</p>
+    <p>Each &lt;page&gt; element will start on a new page in the PDF and may span multiple pages in the resulting PDF document.</p>
+    <p>Each &lt;newPage&gt; element will start on a new page and will include the &lt;header&gt; and &lt;footer&gt; of it's parent.</p>
     <p>&lt;header&gt; and &lt;footer&gt; elements are included in each page their parent block is in.</p>
     <Pre>{pre}</Pre>
+    <p>&lt;block&gt;elements will not be split unless they are too big to fit on a single page, including any headers or footers.</p>
   </newPage>
 );
 
